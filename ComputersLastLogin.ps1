b65b89ccbc00a0f61819c4f158ed8a41aa8dd248
Daniel Haden <haden.daniel@gmail.com>
2019-05-18 12:00:05 -0600

    finds computer objects that haven't logged on in X days

---
+++ ComputersLastLogin.ps1
@@ -0,0 +1,15 @@
+﻿$OUpath = Get-Content "C:\Users\hadend\Desktop\scripts\info.txt" | 
+            Select-Object -First 1
+            
+$curDir = Get-Location
+Set-Location AD:\
+
+
+$DaysInactive = Read-Host -Prompt "List computers inactive for more than how many days"
+
+$time = (Get-Date).AddDays(-($DaysInactive))
+
+Get-ADComputer -Filter {(LastLogonDate -lt $time)} -SearchBase $OUpath -Properties * |
+    Format-Table SamAccountName, LastLogonDate
+
+Set-Location $curDir
