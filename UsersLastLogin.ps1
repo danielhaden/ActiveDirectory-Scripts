4cebd011d9109a61d204411c39d911fb0eb8ca6e
Daniel Haden <haden.daniel@gmail.com>
2019-05-18 09:25:10 -0600

    Generates list of inactive users older than X number of days

---
+++ UsersLastLogin.ps1
@@ -0,0 +1,18 @@
+﻿#Finds accounts inactive longer than X days
+
+$OUpath = Get-Content "C:\Users\hadend\Desktop\scripts\info.txt" | 
+            Select-Object -First 1
+
+
+$curDir = Get-Location
+Set-Location AD:\
+
+
+$DaysInactive = Read-Host -Prompt "List users inactive for more than how many days"
+
+$time = (Get-Date).AddDays(-($DaysInactive))
+
+Get-ADUser -Filter {(LastLogonDate -lt $time) -and (Enabled -eq $TRUE)} -SearchBase $OUpath -Properties * |
+    Format-Table SamAccountName, UserPrincipalName, LastLogonDate
+
+Set-Location $curDir
