5d4d4fbdc86779c508f32f1841bf1b95cc1f9392
Daniel Haden <haden.daniel@gmail.com>
2019-05-17 21:31:51 -0600

    search AD users by name

---
+++ UserFromName.ps1
@@ -0,0 +1,16 @@
+﻿$curDir = Get-Location
+Set-Location AD:\
+
+If ($args.Count -eq 1) {
+    $lastName = $args[0]
+    Get-ADUser -filter {(SurName -eq $lastName)}
+
+} Elseif ($args.Count -eq 2) {
+    $firstName = $args[0]
+    $lastName = $args[1]
+    Get-ADUser -filter {(GivenName -eq $firstName) -and 
+                        (SurName -eq $lastName)}
+
+}
+
+Set-Location $curDir
